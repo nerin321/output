edfd4ec41168647c90d5af019017549e4ab51dae
nerin321 <lethanhtuangam@gmail.com>
2024-12-03 09:00:16 +0700

    fix new

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  assetPrefix: "./", // Sử dụng ./ để đường dẫn tương đối
-};
-
-module.exports = nextConfig;
